b8142ad50fee3fe31c9c57eae7d79d536ebf29cb
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-01-11 23:09:27 +0530

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,12 +1,11 @@
 def get_number(number):
-    while True:
-        operand = input("Number " + str(number) + ": ")
-        try:
-            return float(operand)
-        except:
-            print("Invalid number, try again.")
-operand = get_number(1)
-operand2 = get_number(2)
+    operand=input("number 1 "+ str(number)+ ":")
+    try:
+        return float(operand)
+    except:
+        print("Invalid number , Try agin")
+operand=get_number(1)
+operand2=get_number(2)
 sign = input("Sign: ")
 result = 0
 if sign == "+":
